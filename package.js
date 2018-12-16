94a40da51627c6322afca50a5b1f4aa55c7ce7bf
Lauri Ojansivu <x@xet7.org>
2018-12-16 10:56:44 +0200

    - Use jquery.mousewheel.js locally. Not loading from CDN anymore.
    Thanks to xet7 !

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A wrapper for Malihu Custom Scrollbar. Highly customizable custom scrollbar jQuery plugin",
   version: "3.1.3",
-  git: "https://github.com/wekan/wekan-scrollbar.git"
+  git: "https://github.com/MaazAli/Meteor-Malihu-Custom-Scrollbar.git"
 });
 
 Package.onUse(function(api) {
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.use('jquery');
 
   // JS
-
+  api.addFiles('jquery.mousewheel.js', 'client');
   api.addFiles('jquery.mCustomScrollbar.js', 'client');
 
   // CSS
